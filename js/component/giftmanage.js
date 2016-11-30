8036dc76e8b51b4c9b60474853fb8e512920168d
283731869@qq.com <28373869@qq.com>
2016-11-30 17:25:17 +0800

    修复礼品管理上传图片

--- js/component/giftmanage.js
+++ js/component/giftmanage.js
@@ -1,7 +1,7 @@
 /**
  * Created by ASSOON on 2016/11/9.
  */
-define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack'], function(bootstrap,avalon,jstree,jquery_select,swal,featurepack) {
+define(['bootstrap','avalon','cropbox','sweet_alert','featurepack'], function(bootstrap,avalon,cropbox,swal,featurepack) {
     var giftManage,dataUrl;
     var postdata = {gifname:""};
     var cloudMail = {
@@ -23,6 +23,9 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
                 editGift:function (el) {
                     giftManage.globalData ={type:2};
                     cloudMail.Judgment(2,el);
+                },
+                uploadimg:function () {
+                    $("#file").click();
                 },
                 downGift:function (el) {
                     swal({
@@ -48,7 +51,7 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
                         c_totalfee:giftManage.filldata.c_totalfee,
                         gift_stock:giftManage.filldata.gift_stock,
                         description:giftManage.filldata.description,
-                        gift_img:"img/profileimg.png"//还没有写插件
+                        gift_img:giftManage.filldata.gift_img
                     };
                     giftManage.globalData.type ==1?(function () {
                         postdata.giftid = 0;
@@ -75,13 +78,13 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
             })():(function () {
                 giftManage.filldata = value;
             })();
-            $('#showBigBox').click();
+            $('#showBigBox20').click();
         },
         editGift:function (postdata) {
             featurepack.pack.ajax(dataUrl.editPayUrl, "post", postdata, function (result) {
                 if (result.code == 0) {
                     swal("修改成功!", "您已经修改了该成员，点击OK关闭窗口。", "success");
-                    $("#showBigBox").click();
+                    $("#showBigBox20").click();
                 } else {
                     swal(result.msg, "", "error");
                 }
@@ -92,11 +95,14 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
             featurepack.pack.ajax(dataUrl.addPayUrl, "get", postdata, function (result) {
                 if (result.code == 0) {
                     swal("添加成功!", "您已经添加成员，点击OK关闭窗口。", "success");
-                    $("#showBigBox").click();
+                    $("#showBigBox20").click();
                 } else {
                     swal(result.msg, "", "error");
                 }
             })
+        },
+        getdata:function () {
+            imgData64 = arguments[0];
         },
         downGift:function () {
             featurepack.pack.ajax(dataUrl.refundMoneyUrl, "get", postdata, function (result) {
@@ -106,13 +112,31 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
                     swal(result.msg, "", "error");
                 }
             })
+        },
+        savaimage:function (postdata) {
+            featurepack.pack.ajax(dataUrl.saveImageUrl,"post",postdata,function (result) {
+                if(result.code == 0){
+                    $('.loader').fadeOut(200);
+                    giftManage.filldata.gift_img = result.data.url;
+                    imgData64 = null;
+                    $('#modalicon').click();
+                    swal("保存成功!", "您已经成功修改了这张图片，点击OK关闭窗口。", "success");
+                }else{
+                    $('#modalicon').click();
+                    swal(result.msg,"", "error");
+                    $('.loader').fadeOut(200);
+
+                }
+            })
         }
     };
     var initStart = function (l) {
         dataUrl = l;
+        $('#saveimg').on('click',function () {cloudMail.savaimage({imageData:imgData64});$('.loader').fadeIn(100)});
         featurepack.pack.toggleTops();
         cloudMail.getResponse(postdata);
         cloudMail.avalonStart();
+        featurepack.pack.cropimage(cloudMail.getdata);
     };
     return {
         init_start: initStart
