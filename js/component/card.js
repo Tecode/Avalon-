80fce55645a9c400f9736d032e74aa6a50d84e59
283731869@qq.com <28373869@qq.com>
2016-12-02 17:57:36 +0800

    会员卡页面

--- js/component/card.js
+++ js/component/card.js
@@ -3,87 +3,16 @@
  */
 define(['avalon','bootstrap','bootstrap_select','moment','daterangepicker','featurepack','sweet_alert'], function(avalon,bootstrap,selectpicker,moment,daterangepicker,featurepack,swal) {
     var dataUrl = null;
-    var showList,searchList;
-    var postdata = {
-        starttime:"",
-        endtime:"",
-        cardname:"",
-        cardtype:""
-    };
+    var vipCard;
     var cloudMail = {
         avalonStart:function () {
-            showList = avalon.define({
-                $id:"showList",
-                listData:[],
-                filldata:{},
-                deadline:'',//截至时间
-                discount:'',//打几折
-                reducesum:'',//减多少元
-                deleteCard:function (el) {
-                    swal({
-                            title: "确定要删除吗？?",
-                            text: "删除以后将不会恢复!",
-                            type: "warning",
-                            showCancelButton: true,
-                            cancelButtonText:"取消",
-                            confirmButtonColor: "#DD6B55",
-                            confirmButtonText: "确定",
-                            closeOnConfirm: false
-                        },
-                        function(){
-                            cloudMail.deleteCard({cardclomid:el.cardclomid});
-                        });
-                },
-                editCard:function (el) {
-                    cloudMail.Judgment(2,el);
-                    globalData =2;
-                },
-                clearAttr:function () {
-                    $('.tips-msg').remove()
-                },
-                validate: featurepack.pack.checkValue(function () {
-                    var postdata ={
-                        cardname:showList.filldata.cardname,
-                        cardsums:showList.filldata.cardsums,
-                        cardtype:showList.filldata.cardtype,
-                        deadline:showList.deadline,
-                        discount:showList.discount,
-                        reducesum:showList.reducesum
-                    };
-                    globalData==1?(function () {
-                        postdata.cardclomid = 0;
-                        cloudMail.addCard(postdata);
-                    })():function () {
-                        postdata.cardclomid = showList.filldata.cardclomid;
-                        cloudMail.editCard(postdata);
-                    }()
-                })
-            });
+            vipCard = avalon.define({
 
-            searchList = avalon.define({
-                $id:"searchList",
-                addCard:function () {
-                    cloudMail.Judgment(1,null);
-                    globalData = 1;
-                },
-                searchButton:function () {
-                    $("form").serializeArray().map(function (child,index) {
-                        postdata[child.name] = child.value
-                    });
-                    cloudMail.getResponse(postdata);
-                }
             });
             avalon.scan(document.body);
         },
         Judgment:function (type,value) {
-            type==1?(function () {
-                showList.filldata = {cardname:"",cardtype:"1",cardsums:"",arrivsum:""};
-                showList.arrivsum ="";//减多少
-                showList.discount ="";//打几折
-                showList.deadline ="";
-            })():(function () {
-                showList.filldata = value;
-            })();
+
             $('#showBigBox').click();
         },
         //分页插件封装的avalon需要传url
@@ -129,14 +58,14 @@ define(['avalon','bootstrap','bootstrap_select','moment','daterangepicker','feat
     };
     var initStart = function (url) {
         dataUrl = url;
-        //下拉选项初始化
-        featurepack.pack.option();
-        featurepack.pack.toggleTops();
-        featurepack.pack.datePicker(cloudMail.getTime,"#date-range-picker",false);
-        featurepack.pack.datePicker(cloudMail.getTime,"#date-picker",true);
-        //分页和查询
-        cloudMail.getResponse(postdata);
-        cloudMail.avalonStart();
+        // //下拉选项初始化
+        // featurepack.pack.option();
+        // featurepack.pack.toggleTops();
+        // featurepack.pack.datePicker(cloudMail.getTime,"#date-range-picker",false);
+        // featurepack.pack.datePicker(cloudMail.getTime,"#date-picker",true);
+        // //分页和查询
+        // cloudMail.getResponse(postdata);
+        // cloudMail.avalonStart();
 
     };
     return {
