e0b6e45ab56a0c68d566370980209d19f14bb8bf
283731869@qq.com <28373869@qq.com>
2016-12-03 19:09:37 +0800

    '后台页面已经完成'

--- js/component/card.js
+++ js/component/card.js
@@ -1,53 +1,54 @@
 /**
  * Created by ASSOON on 2016/11/18.
  */
-define(['avalon','bootstrap','bootstrap_select','moment','daterangepicker','featurepack','plupload','sweet_alert'], function(avalon,bootstrap,selectpicker,moment,daterangepicker,featurepack,plupload,swal) {
+define(['avalon', 'bootstrap', 'bootstrap_select', 'moment', 'daterangepicker', 'featurepack', 'plupload', 'sweet_alert'], function (avalon, bootstrap, selectpicker, moment, daterangepicker, featurepack, plupload, swal) {
     var dataUrl = null;
     var colorsValue = "Color010Color020Color030Color040Color050Color060Color070Color080Color081Color082";
-    var colors = ["#63b359","#2c9f67","#509fc9","#5885cf","#9062c0","#d09a45","#e4b138","#ee903c","#f08500","#a9d92d"];
+    var colors = ["#63b359", "#2c9f67", "#509fc9", "#5885cf", "#9062c0", "#d09a45", "#e4b138", "#ee903c", "#f08500", "#a9d92d"];
     var vipCard;
     var cloudMail = {
-        avalonStart:function () {
+        avalonStart: function () {
             vipCard = avalon.define({
-                $id:"vipcard",
-                visibleAttr:'hidden',//visible
-                bgColor:'#63b359',
-                vipData:{
-                    c_cardType:'2',//1背景是图片，2背景是颜色
-                    c_joinName:$('.profilebox b').text(),//公司名称
-                    c_joinTitle:'',//会员卡类型，例如：黄金会员
-                    c_joinLogoUrl:'http://paytest.51byod.cn/300.jpg',//logo图片地址
-                    c_joinColor:'Color010',//会员卡背景颜色
-                    c_imageUrl:'img/example1.jpg',//会员卡背景图片地址
-                    c_joinQuantity:'500',//制券数量
-                    c_joinObj6:'',//特权说明
-                    c_joinTel:'',//电话号码
-                    c_joinDescription:'',//卡券说明
-                    c_joinNotice:''//用户须知
+                $id: "vipcard",
+                visibleAttr: 'hidden',//visible
+                bgColor: '#63b359',
+                vipData: {
+                    joinid:0,
+                    c_cardType: '2',//1背景是图片，2背景是颜色
+                    c_joinName: $('.profilebox b').text(),//公司名称
+                    c_joinTitle: '',//会员卡类型，例如：黄金会员
+                    c_joinLogoUrl: 'http://paytest.51byod.cn/300.jpg',//logo图片地址
+                    c_joinColor: 'Color010',//会员卡背景颜色
+                    c_imageUrl: 'img/example1.jpg',//会员卡背景图片地址
+                    c_joinQuantity: '500',//制券数量
+                    c_joinObj6: '',//特权说明
+                    c_joinTel: '',//电话号码
+                    c_joinDescription: '',//卡券说明
+                    c_joinNotice: ''//用户须知
                 },
-                enterdata:[],//入口
-                validate:{
+                enterdata: [],//入口
+                validate: {
                     onValidateAll: function (reasons) {
                         reasons.length == 0 ? (function () {
                             var pData = {};
-                            for (key in vipCard.vipData){
+                            for (key in vipCard.vipData) {
                                 pData[key] = vipCard.vipData[key]
                             }
-                            if(pData.c_imageUrl.length>100){
-                                pData.c_imageUrl ='';
+                            if (pData.c_imageUrl.length > 100) {
+                                pData.c_imageUrl = '';
                             }
                             pData.enterdata = JSON.stringify(vipCard.enterdata);
                             console.info(pData)
                         })() : (function () {
                             $('.tips-msg').remove();
-                            $.each(reasons,(function (index,child) {
+                            $.each(reasons, (function (index, child) {
                                 if ($(child.element).parent('div').hasClass('col-sm-6')) {
                                     $(child.element).parent('div').after('<p class="col-sm-offset-2 tips-msg col-sm-6 color-down">' + child.message + '</p>');
                                 } else if ($(child.element).parent('div').hasClass('col-sm-10')) {
                                     $(child.element).parent('div').after('<p class="col-sm-offset-2 tips-msg col-sm-10 color-down">' + child.message + '</p>');
                                 }
                             }));
-                            swal("提交失败！","请检查填写字段是否正确！", "error");
+                            swal("提交失败！", "请检查填写字段是否正确！", "error");
                         })();
                     },
                     validateInBlur: true,
@@ -56,24 +57,24 @@ define(['avalon','bootstrap','bootstrap_select','moment','daterangepicker','feat
                 clearAttr: function (e) {
                     $(e.target).parents('.col-sm-6,.col-sm-10').siblings('.tips-msg').remove();
                 },
-                addEnter:function () {
-                    vipCard.enterdata.length<3?(function () {
+                addEnter: function () {
+                    vipCard.enterdata.length < 3 ? (function () {
                         vipCard.enterdata.push({
-                            "tips":"",
-                            "rkname":"入口名称",
-                            "joinUrl":""
+                            "tips": "",
+                            "rkname": "入口名称",
+                            "joinUrl": ""
                         })
-                    })():(function () {
-                        swal("添加入口失败！","最多只可以添加3个入口！", "error");
+                    })() : (function () {
+                        swal("添加入口失败！", "最多只可以添加3个入口！", "error");
                     })()
                 },
-                deletEnter:function (el) {
+                deletEnter: function (el) {
                     vipCard.enterdata.remove(el);
                 },
-                toggleBg:function (e) {
-                    if($(e.target).val()==2){
+                toggleBg: function (e) {
+                    if ($(e.target).val() == 2) {
                         vipCard.visibleAttr = 'hidden';
-                    }else if($(e.target).val() ==1) {
+                    } else if ($(e.target).val() == 1) {
                         vipCard.visibleAttr = 'visible';
                     }
                 }
@@ -82,38 +83,63 @@ define(['avalon','bootstrap','bootstrap_select','moment','daterangepicker','feat
         },
 
         //回调函数预览图片
-        callback:function () {
+        callback: function () {
             vipCard.vipData.c_imageUrl = arguments[0];
         },
         //回调函数加载正式图片地址
-        callBackGetUrl:function () {
+        callBackGetUrl: function () {
             vipCard.vipData.c_imageUrl = arguments[0].data.url;
             swal("图片上传成功!", "您已经成功上传了背景图片，点击OK关闭窗口。", "success");
             // globalData.url = true;
         },
-        jqueryInit:function () {
-            $('body').on('click','.topColors div,.bottomColors div',function () {
+        getResponse:function () {
+            featurepack.pack.ajax(dataUrl.getResponseUrl, "get", postdata, function (result) {
+                if (result.code == 0) {
+                    //参数有问题和上面的一样
+                    vipCard.vipData = result.data.vipData;
+                    vipCard.enterdata = result.data.enterdata;
+                } else {
+                    swal(result.msg, "", "error");
+                }
+            })
+        },
+        jqueryInit: function () {
+            $('body').on('click', '.topColors div,.bottomColors div', function () {
                 vipCard.vipData.c_joinColor = $(this).attr('data');
-                vipCard.bgColor = colors[colorsValue.indexOf($(this).attr('data'))/8];
+                vipCard.bgColor = colors[colorsValue.indexOf($(this).attr('data')) / 8];
             });
             //弹出颜色块
             $(function () {
                 $('[data-toggle="popover"]').popover(
                     {
-                        html:true
+                        html: true
                     }
                 )
             });
+        },
+        // 获取地址栏参数
+        getQueryString: function (name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null)return unescape(r[2]);
+            return null;
         }
     };
     var initStart = function (url) {
+        try {
+            alert(getQueryString(joincode));
+            if(getQueryString('joincode') == 'joincode'){
+            }
+        }catch (e){
+
+        }
         dataUrl = url;
         cloudMail.jqueryInit();
         featurepack.pack.upload(
             //转成64位编码
             cloudMail.callback,
             //获取服务器地址
-            cloudMail.callBackGetUrl,url.addBackgroundImageUrl);
+            cloudMail.callBackGetUrl, url.addBackgroundImageUrl);
         cloudMail.avalonStart();
 
     };
--- js/component/wechatcard.js
+++ js/component/wechatcard.js
@@ -1,33 +1,31 @@
 /**
  * Created by ASSOON on 2016/11/18.
  */
-define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack'], function(bootstrap,avalon,jstree,jquery_select,swal,featurepack) {
-    var wechatCard,dataUrl;
-    var postdata = {joinname:""};
+define(['bootstrap', 'avalon', 'sweet_alert', 'featurepack'], function (bootstrap, avalon, swal, featurepack) {
+    var wechatCard, dataUrl;
+    var colorsValue = "Color010Color020Color030Color040Color050Color060Color070Color080Color081Color082";
+    var colors = ["#63b359", "#2c9f67", "#509fc9", "#5885cf", "#9062c0", "#d09a45", "#e4b138", "#ee903c", "#f08500", "#a9d92d"];
+    var postdata = {joinname: ""};
     var cloudMail = {
-        avalonStart:function () {
+        avalonStart: function () {
             wechatCard = avalon.define({
-                $id:"wechatcard",
-                joinname:"",
-                wechatCardList:[],
-                filldata:{},
-                globalData:{},
-                searchButton:function () {
+                $id: "wechatcard",
+                joinname: "",
+                wechatCardList: [],
+                filldata: {},
+                globalData: {},
+                searchButton: function () {
                     postdata.gifname = wechatCard.gifname;
                     cloudMail.getResponse(postdata);
                 },
-                addGift:function () {
-                    cloudMail.Judgment(1,null);
-                    wechatCard.globalData ={type:1}
+                editCard: function () {
+                    //编辑跳转地址带上参数
+                    window.location.href ="#/card"
                 },
-                editGift:function (el) {
-                    wechatCard.globalData ={type:2};
-                    cloudMail.Judgment(2,el);
-                },
-                downGift:function (el) {
+                deleteCard: function (el) {
                     swal({
-                            title: "确定下架吗？?",
-                            text: "您将会下架该商品!",
+                            title: "确定删除吗?",
+                            text: "您将会删除该会员卡!",
                             type: "warning",
                             showCancelButton: true,
                             confirmButtonColor: "#DD6B55",
@@ -36,24 +34,24 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
                             closeOnConfirm: false
                         },
                         function () {
-                            cloudMail.downGift(el.giftid);
+                            cloudMail.deleteCard({joinid:el.joinid});
                         });
                 },
-                clearAttr:function () {
+                clearAttr: function () {
                     $('.tips-msg').remove()
                 },
                 validate: featurepack.pack.checkValue(function () {
-                    var postdata ={
-                        gift_name:wechatCard.filldata.gift_name,
-                        c_totalfee:wechatCard.filldata.c_totalfee,
-                        gift_stock:wechatCard.filldata.gift_stock,
-                        description:wechatCard.filldata.description,
-                        gift_img:"img/profileimg.png"//还没有写插件
+                    var postdata = {
+                        gift_name: wechatCard.filldata.gift_name,
+                        c_totalfee: wechatCard.filldata.c_totalfee,
+                        gift_stock: wechatCard.filldata.gift_stock,
+                        description: wechatCard.filldata.description,
+                        gift_img: "img/profileimg.png"//还没有写插件
                     };
-                    wechatCard.globalData.type ==1?(function () {
+                    wechatCard.globalData.type == 1 ? (function () {
                         postdata.giftid = 0;
                         cloudMail.addGift(postdata);
-                    })():function () {
+                    })() : function () {
                         postdata.giftid = wechatCard.filldata.giftid;
                         cloudMail.editGift(postdata);
                     }()
@@ -65,44 +63,21 @@ define(['bootstrap','avalon','jstree','jquery_select','sweet_alert','featurepack
         getResponse:function (postdata) {
             featurepack.pack.ajax(dataUrl.getwechatcadList, "get", postdata, function (result) {
                 if (result.code == 0) {
-                    wechatCard.wechatCardList = result.data.joincardlist;
+                    var resultData = [];
+                    $.each(result.data.joincardlist,function (index,child) {
+                        child.color = colors[colorsValue.indexOf(child.color) / 8];
+                        resultData.push(child)
+                    });
+                    wechatCard.wechatCardList = resultData;
                 } else {
                     swal(result.msg, "", "error");
                 }
             })
         },
-        Judgment:function (type,value) {
-            type==1?(function () {
-                wechatCard.filldata = {gift_name:"",c_totalfee:"",gift_stock:"",description:"",gift_img:"img/profileimg.png"};
-            })():(function () {
-                wechatCard.filldata = value;
-            })();
-            $('#showBigBox').click();
-        },
-        editGift:function (postdata) {
-            featurepack.pack.ajax(dataUrl.editPayUrl, "post", postdata, function (result) {
+        deleteCard: function (d) {
+            featurepack.pack.ajax(dataUrl.refundMoneyUrl, "get", d, function (result) {
                 if (result.code == 0) {
-                    swal("修改成功!", "您已经修改了该成员，点击OK关闭窗口。", "success");
-                    $("#showBigBox").click();
-                } else {
-                    swal(result.msg, "", "error");
-                }
-            })
-        },
-        addGift:function (postdata) {
-            featurepack.pack.ajax(dataUrl.addPayUrl, "get", postdata, function (result) {
-                if (result.code == 0) {
-                    swal("添加成功!", "您已经添加成员，点击OK关闭窗口。", "success");
-                    $("#showBigBox").click();
-                } else {
-                    swal(result.msg, "", "error");
-                }
-            })
-        },
-        downGift:function () {
-            featurepack.pack.ajax(dataUrl.refundMoneyUrl, "get", postdata, function (result) {
-                if (result.code == 0) {
-                    swal("下架成功!", "您已经成功下架此商品，点击OK关闭窗口。", "success");
+                    swal("删除!", "您已经成功删除了此会员卡，点击OK关闭窗口。", "success");
                 } else {
                     swal(result.msg, "", "error");
                 }
